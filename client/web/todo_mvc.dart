6f47d40f19d408b41b3f2fd550db8e28b1a1f8ba
Dzenan Ridjanovic <dzenanr@gmail.com>
2014-04-30 12:16:14 -0400

    add getresponse to local and DartVoid url

--- client/web/todo_mvc.dart
+++ client/web/todo_mvc.dart
@@ -3,8 +3,8 @@ import 'package:todo_mvc/todo_mvc_app.dart';
 
 const String HOST = '127.0.0.1'; // eg: localhost
 const int PORT = 8080;
-//String url = 'http://${HOST}:${PORT}';
-String url = 'http://todomvcmongodb.dzenanr.dartblob.com/';
+//String url = 'http://${HOST}:${PORT}/getresponse';
+String url = 'http://todomvcmongodb.dzenanr.dartblob.com/getresponse';
 
 main() {
   var repository = new Repository();
